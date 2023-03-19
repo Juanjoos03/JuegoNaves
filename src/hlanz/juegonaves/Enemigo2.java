b510ac256d1b98b206d77154c6e3bf4545e55ec4
ArekkMirmun <alexmmonline@gmail.com>
2023-03-19 21:02:48 +0100

    Elimino imports innecesarios

--- src/hlanz/juegonaves/Enemigo2.java
+++ src/hlanz/juegonaves/Enemigo2.java
@@ -4,34 +4,35 @@
  */
 package hlanz.juegonaves;
 
-import java.util.Random;
-
 /**
  *
  * @author Juanjoos
  */
-public class Enemigo2 extends Enemigo{
+public class Enemigo2 extends Enemigo {
     private int centroX;
     private int centroY;
     private int radio;
     private double angulo;
     private double velocidad;
-    public Enemigo2(int x,int y,int cx,int cy,int r,int v){
-        super(x,y,CacheImagenes.getInstancia().getImagen("malo2.png"));
-        centroX=cx;
-        centroY=cy;
-        radio=r;
-        velocidad=v;
-        angulo=0;
+
+    public Enemigo2(int x, int y, int cx, int cy, int r, int v) {
+        super(x, y, CacheImagenes.getInstancia().getImagen("malo2.png"));
+        centroX = cx;
+        centroY = cy;
+        radio = r;
+        velocidad = v;
+        angulo = 0;
     }
+
     @Override
     public int getPuntuacion() {
-        return (int)Math.abs(velocidad * 2);
+        return (int) Math.abs(velocidad * 2);
     }
 
     @Override
     public void ejecutarFrame() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+                                                                       // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
--- src/hlanz/juegonaves/Nave2.java
+++ src/hlanz/juegonaves/Nave2.java
@@ -4,32 +4,31 @@
  */
 package hlanz.juegonaves;
 
-import java.io.File;
-import javax.imageio.ImageIO;
-
 /**
  *
  * @author Juanjoos
  */
-public class Nave2 extends Nave{
-    
-    public Nave2(int ti,int tde,int tdi){
-        super(ti,tde,tdi,3,400,900,CacheImagenes.getInstancia().getImagen("nave2.png"));
+public class Nave2 extends Nave {
+
+    public Nave2(int ti, int tde, int tdi) {
+        super(ti, tde, tdi, 3, 400, 900, CacheImagenes.getInstancia().getImagen("nave2.png"));
     }
-    
+
     /*
-    se creará y añadirá a la escena del juego un objeto Disparo en las coordenadas (x,y) de la nave con una
-    velocidad de 0 píxeles en el eje x y -10 píxeles en el eje Y (con esto conseguimos que el disparo se mueva hacia arriba). El
-    disparo creado se añadirá a la escena.
-    */
+     * se creará y añadirá a la escena del juego un objeto Disparo en las
+     * coordenadas (x,y) de la nave con una
+     * velocidad de 0 píxeles en el eje x y -10 píxeles en el eje Y (con esto
+     * conseguimos que el disparo se mueva hacia arriba). El
+     * disparo creado se añadirá a la escena.
+     */
     @Override
     protected void realizarDisparo() {
-        Disparo d=new Disparo(1,this.getX()+60,this.getY()-20,-3,-3);
-        Disparo d2=new Disparo(1,this.getX()+60,this.getY()-20,3,-3);
+        Disparo d = new Disparo(1, this.getX() + 60, this.getY() - 20, -3, -3);
+        Disparo d2 = new Disparo(1, this.getX() + 60, this.getY() - 20, 3, -3);
         this.juego.getEscena().añadir(d);
         this.juego.getEscena().añadir(d2);
-        //d.inicializar();
-        //d2.inicializar();
+        // d.inicializar();
+        // d2.inicializar();
     }
-    
+
 }
