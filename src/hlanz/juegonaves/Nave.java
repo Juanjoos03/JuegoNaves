be2356385d8663c7dc52d3f90461f5911a5c652b
ArekkMirmun <alexmmonline@gmail.com>
2023-03-19 21:00:00 +0100

    Reducida la velocidad de disparo de la nave

--- src/hlanz/juegonaves/Nave.java
+++ src/hlanz/juegonaves/Nave.java
@@ -8,49 +8,51 @@ import java.awt.Image;
 import java.time.Instant;
 import bpc.framework.consola.*;
 import java.time.Duration;
+
 /**
  *
  * @author Juanjoos
  */
-public abstract class Nave extends SpriteGameObject{
+public abstract class Nave extends SpriteGameObject {
     private int teclaIzquierda;
     private int teclaDerecha;
     private int teclaDisparo;
     private int velocidad;
     private Instant ultimoDisparoRealizado;
-    
-    public Nave(int ti,int tde,int tdi,int v,int x,int y,Image imagen){
-        super(imagen,x,y);
-        teclaIzquierda=ti;
-        teclaDerecha=tde;
-        teclaDisparo=tdi;
-        velocidad=v;
-        ultimoDisparoRealizado=Instant.now();
+
+    public Nave(int ti, int tde, int tdi, int v, int x, int y, Image imagen) {
+        super(imagen, x, y);
+        teclaIzquierda = ti;
+        teclaDerecha = tde;
+        teclaDisparo = tdi;
+        velocidad = v;
+        ultimoDisparoRealizado = Instant.now();
     }
+
     @Override
     public void ejecutarFrame() {
-        if(this.consola.getTeclado().teclaPulsada(teclaIzquierda)&&this.getX()>0){
-                this.moverX(-velocidad);
+        if (this.consola.getTeclado().teclaPulsada(teclaIzquierda) && this.getX() > 0) {
+            this.moverX(-velocidad);
         }
-        if(this.consola.getTeclado().teclaPulsada(teclaDerecha)&&this.getX()+this.getAnchura()<this.juego.getAnchuraPantalla()){
+        if (this.consola.getTeclado().teclaPulsada(teclaDerecha)
+                && this.getX() + this.getAnchura() < this.juego.getAnchuraPantalla()) {
             this.moverX(+velocidad);
         }
-        if(this.consola.getTeclado().teclaPulsada(teclaDisparo)){
+        if (this.consola.getTeclado().teclaPulsada(teclaDisparo)) {
             this.disparar();
         }
-        
+
     }
-    
-    public void disparar(){
-        Instant now=Instant.now();
-        if(Duration.between( ultimoDisparoRealizado,now).getNano()>200000000){
-            ultimoDisparoRealizado=now;
+
+    public void disparar() {
+        Instant now = Instant.now();
+        if (Duration.between(ultimoDisparoRealizado, now).getNano() > 350000000) {
+            ultimoDisparoRealizado = now;
             this.realizarDisparo();
         }
-        //this.realizarDisparo();
+        // this.realizarDisparo();
     }
+
     protected abstract void realizarDisparo();
 
-    
-    
 }
