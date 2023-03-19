e558a5e8340482c5788a459d55ca0cb621619579
ArekkMirmun <alexmmonline@gmail.com>
2023-03-19 22:18:43 +0100

    Corregido metodo deprecado de camara

--- src/hlanz/juegonaves/Fondo.java
+++ src/hlanz/juegonaves/Fondo.java
@@ -12,34 +12,34 @@ import java.awt.Rectangle;
  *
  * @author Juanjoos
  */
-public class Fondo extends GameObject{
+public class Fondo extends GameObject {
     private Image imagen;
     private Rectangle camara;
     private int velocidadCamara;
-    
-    public Fondo(int vc){
-        velocidadCamara=vc;
-        imagen=CacheImagenes.getInstancia().getImagen("fondo.png");
-        camara=new Rectangle();
+
+    public Fondo(int vc) {
+        velocidadCamara = vc;
+        imagen = CacheImagenes.getInstancia().getImagen("fondo.png");
+        camara = new Rectangle();
     }
 
     @Override
     public void inicializar() {
-        camara=new Rectangle(0,0,this.juego.getAnchuraPantalla(),this.juego.getAlturaPantalla());
+        camara = new Rectangle(0, 0, this.juego.getAnchuraPantalla(), this.juego.getAlturaPantalla());
     }
 
     @Override
     public void ejecutarFrame() {
         consola.getCapaCanvas().getGraphics().drawImage(imagen, camara.x, camara.y, null);
-        camara.move(0, velocidadCamara);
-        if(camara.y>=imagen.getHeight(null)){
-            camara.x=0;
-            camara.y=0;
+        camara.setLocation(0, camara.y + velocidadCamara);
+        if (camara.y >= imagen.getHeight(null)) {
+            camara.x = 0;
+            camara.y = 0;
         }
     }
 
     @Override
     public void finalizar() {
-        
+
     }
 }
